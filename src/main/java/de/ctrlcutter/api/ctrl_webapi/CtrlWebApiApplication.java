598d83c52787d4060abd3968e2a5990fb0553dee
Lorenz Seufert <lorenzseufert@gmail.com>
2021-11-04 12:33:04 +0100

    :sparkles: Inital commit

---
+++ src/main/java/de/ctrlcutter/api/ctrl_webapi/CtrlWebApiApplication.java
@@ -0,0 +1,13 @@
+package de.ctrlcutter.api.ctrl_webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtrlWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ctrlcutter/api/ctrl_webapi/CtrlWebApiApplicationTests.java
@@ -0,0 +1,12 @@
+package de.ctrlcutter.api.ctrl_webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
