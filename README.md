b8129185f9580aabfec00887290fb2bed4fcf3ad
Lorenz Seufert <lorenzseufert@gmail.com>
2021-11-27 23:30:22 +0100

    added instructions for application start in README.md and fixed basic auth for API

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # CTRL_Web-API
+
+## Environment variables
+
+Following environment variables are necessary:
+
+- DB_URL - jdbc:mysql://URLTODB:PORT/DBNAME
+- DB_USER - username of the DB account
+- DB_PASSWORD - password of the DB account
+- API_USER - name of the API account
+- API_PASSWORD - password of the API account
+
+## Run application
+
+- Install dependencies ``mvn install``
+- Define environment variables
+- Run Main-Method in CtrlWebApiApplication class
--- src/main/java/com/ctrlcutter/api/ctrl_webapi/WebSecurityConfigurer.java
+++ src/main/java/com/ctrlcutter/api/ctrl_webapi/WebSecurityConfigurer.java
@@ -2,19 +2,22 @@ package com.ctrlcutter.api.ctrl_webapi;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //TODO Entfernen und richtig machen mit authentication!! Only for development!!
+        //Basic authentication with credentials from environment variables
+        //CSRF is disabled!!!
         http
-                .csrf().disable()
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
     }
 }
